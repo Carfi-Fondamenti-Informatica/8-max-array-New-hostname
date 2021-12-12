76a501acdcde913e12a1c028b4df3545e5e7edb7
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-12-12 16:01:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
 int main(){
+  
+  int rango = 0;
+  float num[rango];
+
+  cin >> rango;
+  
+  for(int = 0; i < rango; i++)
+  {
+    cin >> num[i];
+  }
+  
+  Massimo(num, rango);
+  
+  cout << massimo(num, rango) << endl;
+  
   return 0;
 }
