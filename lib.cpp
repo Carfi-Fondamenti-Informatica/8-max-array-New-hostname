f3cfb014111090438a9e87a56b4a8d91925a03e8
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-12-12 16:04:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+float massimo(const float num[], int rango)
+{
+  num[rango];
+  
+  float max = num[0];
+  
+  for(int i = 0; i < rango; i++)
+  {
+    if(max < num[i])
+    {
+      max = num[i];
+    }
+  }
+  
+  return max;
+}
